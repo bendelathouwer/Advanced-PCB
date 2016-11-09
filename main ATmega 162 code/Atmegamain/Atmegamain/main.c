a644a890901077cbb60048d724590eaa0df88a4b
Ben <Tomtom2014>
2016-11-09 13:32:30 +0100

    schema
    added ultrasoon schematic

---
+++ main ATmega 162 code/Atmegamain/Atmegamain/main.c
@@ -0,0 +1,18 @@
+/*
+ * Atmegamain.c
+ *
+ * Created: 11/9/2016 1:11:48 PM
+ * Author : Ecto1
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
