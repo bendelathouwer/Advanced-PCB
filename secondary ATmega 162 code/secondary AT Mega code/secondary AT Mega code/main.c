2d6770596157121756592a55c7b20a626943973b
Ben <Tomtom2014>
2016-11-17 21:59:55 +0100

    code scundaire ATmega
    code voor secundaire ATmega

---
+++ secondary ATmega 162 code/secondary AT Mega code/secondary AT Mega code/main.c
@@ -0,0 +1,40 @@
+/*
+ * secondary AT Mega code.c
+ *
+ * Created: 11/17/2016 9:53:30 PM
+ * Author : Ecto1
+ */ 
+
+#include <avr/io.h>
+
+
+
+void trigUltrasoon(void);
+
+int main(void)
+{
+    
+    while (1) 
+    {
+    }
+}
+
+void trigUltrasoon(void)// trigering the ultrasonic sensor 
+{
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+	asm("nop");
+}
