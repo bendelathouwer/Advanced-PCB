f6ab0840a760bc1b7cf1e99ce406802bc1b6ddec
Ben <Tomtom2014>
2016-12-17 20:50:41 +0100

    verbeterd ppt
    ppt verbeterd volgens de bespreking met de proffen

--- main ATmega 162 code/Atmegamain/Atmegamain/main.c
+++ main ATmega 162 code/Atmegamain/Atmegamain/main.c
@@ -6,58 +6,12 @@
  * main processor code 
  *
  */ 
-
+#define F_CPU 320000000 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdint.h>
 
 
-void trigUltrasoon( void );
-
-void SetLevelTranslatorState ( int );
-
-void EnableEsp8266 ( int );
-
-
-
-
-int main( void )
-{
-	
-	DDRD |= ( 1 << PORTD6);
-	
-	
-	DDRC  |= (1 << PORTC0);
-	
-    while (1) 
-    {
-    }
-}
-
-
-void SetLevelTranslatorState ( int state  )
-{
-	if (state == 0)
-	{
-		PORTC &= ~(1<<PORTC0);
-	}
-	if ( state == 1 )
-	{
-			PORTC |= (1<<PORTC0);
-	}
-	
-}  
-
-void EnableEsp8266 ( int enable )
-{
-	if (enable  == 0)
-	{
-		PORTD &= ~(1<<PORTD6);
-	}
-	if ( enable  == 1 )
-	{
-		PORTD |= (1<<PORTD6);
-	}
-}
+//void trigUltrasoon( void );
 
 
--- secondary ATmega 162 code/secondary AT Mega code/secondary AT Mega code/main.c
+++ secondary ATmega 162 code/secondary AT Mega code/secondary AT Mega code/main.c
@@ -1,43 +1,41 @@
 /*
- * secondary AT Mega code.c
+ * Atmegamain.c
  *
- * Created: 11/17/2016 9:53:30 PM
- * Author : Ecto1
+ * Created: 11/9/2016 1:11:48 PM
+ * Author : Ben De LAthouwer
+ * main processor code 
+ *
  */ 
-
+#define F_CPU 320000000 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdint.h>
 
 
-void trigUltrasoon(void);
-void mesureDistance(void );
-int main(void)
+//void trigUltrasoon( void );
+
+//void SetLevelTranslatorState ( int );
+
+void EnableEsp8266 ( int );
+
+void FastForward ( void );
+
+
+int main( void )
 {
-    
+	DDRA |= (1 << DDA0)| (1<<DDA1) | (1<<DDA2) | (1<<DDA3)|(1<<DDA4) | (1<<DDA5) | (1<<DDA6);// seting the port as output 
+	
     while (1) 
     {
+		FastForward();
     }
 }
 
-void trigUltrasoon(void)// trigering the ultrasonic sensor 
+void FastForward()
 {
-	// trigering ultrasonic sensor 
-	PORTD |= (1<<PORTD6);
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	asm("nop");
-	PORTD &= ~(1<<PORTD6);
+	
+	PORTA |= (1 << PA0)| (1<<PA1) | (1<<PA2) | (1<<PA3)|(1<<PA4) | (1<<PA5) | (1<<PA6);// drives pins A0=>A7 high 
+	
+	
+	
 }
