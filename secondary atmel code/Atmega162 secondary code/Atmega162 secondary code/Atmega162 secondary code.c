71f0e26a26b1df830f0f3fa1294a930ca95bffbb
Ben <Tomtom2014>
2017-01-28 15:12:38 +0100

    poster + ppt + demo code
    added demo code +powerpoint presentation and poster

--- secondary atmel code/Atmega162 secondary code/Atmega162 secondary code/Atmega162 secondary code.c
+++ secondary atmel code/Atmega162 secondary code/Atmega162 secondary code/Atmega162 secondary code.c
@@ -11,46 +11,105 @@
 #include <avr/interrupt.h>
 #include <stdint.h>
 
-// function prototypes
- //void ReadTrottleStick( void );
- //void ReadRudderStick ( void );
- //void NormalizeRudder ( void );
- //void NormalizeTrottle ( void );
- void  GeneratePWM1( int OfTime , int Ontime );
- void  Ge neratePWM2( int OfTime1 , int Ontime1 );
+
+//function prototypes
+void enableMotorDriver ( int enable );
+
+void BotForward ( void );
+void BotBackwards( void  );
+void BotLeft ( void );
+void BotRight ( void  );
+void BotStop  ( void );
+
 
 
 int  main( void )
 {
 	 
+	DDRA|= ( 1<<PORTA0) |( 1<<PORTA1 ) |( 1<<PORTA2 ) |(1<<PORTA3) |(1<<PORTA4) |(1<<PORTA5) |(1<<PORTA6);//setting the port a pins (0 to 6) as an output 
+    
+	PORTA |= (1<<PORTA0);//setting pwmB high
+	PORTA |= (1<<PORTA6);//setting pwmA high
 	
-    while ( 1 ) 
+	
+	while ( 1 ) 
     {
+		int enable = 1;
+		enableMotorDriver( enable );
 		
     }
 }
 
-
-
-// these functions generate  a pwm signal based on the on of timer value's 
-void GeneratePWM1( int OfTime , int Ontime )
-
+void enableMotorDriver ( int enableDriver )
 {
+	if(enableDriver == 1 )
+	{
+		PORTA |= (1<<PORTA3);
+	}
+	else 
+	{
+		PORTA &= ~(1<<PORTA3);
+	}
 	
 }
 
-void GeneratePWM2(  int OfTime1 , int Ontime1  )
-
+// both motors clockwise
+void BotForward ( void )
 {
+	PORTA &= ~(1<<PORTA1);//Bin1 pin
+	PORTA |= (1<<PORTA2);//Bin2 pin
+	PORTA |= (1<<PORTA3);//standby pin
+	PORTA &= ~(1<<PORTA4);//Ain1 pin
+	PORTA |= (1<<PORTA5);//Ain2  pin 
+	
+
+}
+
+// bothe motors counter clockwise
+void BotBackwards( void  )
+{
+	PORTA |= (1<<PORTA1);//Bin1 pin
+	PORTA &= ~(1<<PORTA2);//Bin2 pin
+	PORTA |= (1<<PORTA3);//standby pin
+	PORTA |= (1<<PORTA4);//Ain1 pin
+	PORTA &= ~(1<<PORTA5);//Ain2  pin
+
+	
+		
+		
+}
+
+//  motor1 clockewise , motor2 counter clockwise 
+void BotLeft ( void )
+{
+	PORTA &= ~(1<<PORTA1);//Bin1 pin
+	PORTA |= (1<<PORTA2);//Bin2 pin
+	PORTA |= (1<<PORTA3);//standby pin
+	PORTA |= (1<<PORTA4);//Ain1 pin
+	PORTA &= ~(1<<PORTA5);//Ain2  pin
+	 
+	
 	
 }
 
-//void ReadRudderStick()
-//{
-//	
-//	
-//}
-//
-//void ReadTrottleStick()
-//{
-//}
+//  motor1 clockewise , motor2 counter clockwise 
+void BotRight ( void  )
+{
+	PORTA |=(1<<PORTA1);//Bin1 pin
+	PORTA &= ~(1<<PORTA2);//Bin2 pin
+	PORTA |= (1<<PORTA3);//standby pin
+	PORTA |= (1<<PORTA4);//Ain1 pin
+	PORTA &= ~(1<<PORTA5);//Ain2  pin
+	
+		
+}
+// both motors off
+void BotStop  ( void )
+{
+		PORTA |=(1<<PORTA1);//Bin1 pin
+		PORTA |= (1<<PORTA2);//Bin2 pin
+		PORTA &= ~(1<<PORTA3);//standby pin
+		PORTA |= (1<<PORTA4);//Ain1 pin
+		PORTA |= (1<<PORTA5);//Ain2  pin
+}
+
