ecb72b89cb5866bc1bbb4ad163945dea86fbc3d0
Ben <Tomtom2014>
2017-01-29 10:58:55 +0100

    poster + working democode
    finised demo code

--- secondary atmel code/Atmega162 secondary code/Atmega162 secondary code/Atmega162 secondary code.c
+++ secondary atmel code/Atmega162 secondary code/Atmega162 secondary code/Atmega162 secondary code.c
@@ -6,105 +6,103 @@
  * main processor code 
  *
  */ 
-#define F_CPU 320000000 
+#define F_CPU 320000000UL 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdint.h>
 #include <util/delay.h>
 
-
-//function prototypes
-
-void BotForward ( void );
-void BotBackwards( void  );
-void BotLeft ( void );
-void BotRight ( void  );
-void BotStop  ( void );
-
+ 
+ 
 
 
 int  main( void )
 {
-	 
-	DDRA|= ( 1<<PORTA0) |( 1<<PORTA1 ) |( 1<<PORTA2 ) |(1<<PORTA3) |(1<<PORTA4) |(1<<PORTA5) |(1<<PORTA6);//setting the port a pins (0 to 6) as an output 
-    
-	PORTA |= (1<<PORTA0);//setting pwmB high
-	PORTA |= (1<<PORTA6);//setting pwmA high
+	DDRA|= ( 1<<PORTA0) |( 1<<PORTA1 ) |( 1<<PORTA2 ) |(1<<PORTA3) |(1<<PORTA4) |(1<<PORTA5) |(1<<PORTA6);//setting the port a pins (0 to 6) as an output
+
+	PORTA = 0;
 	
-	
-	while ( 1 ) 
-    {
-		BotForward();
-		_delay_ms(5000);
-		BotBackwards();
-		_delay_ms(5000);
-		BotLeft();
-		_delay_ms(5000);
-		BotRight();
-		_delay_ms(5000);
-		BotStop();
-		_delay_ms(5000);
+	setPortABit(PA0); // setting pwmB high
+	setPortABit(PA3); // standby pin high
+	setPortABit(PA6); // setting pwmA high
 		
+	while ( 1 ) 
+    {	
+		botForward();
+		wait(3);
+		
+		botLeft();
+		wait(3);
+		
+		botForward();
+		wait(3);
+		
+		botRight();
+		wait(3);
+		
+		botForward();
+		wait(3);
+		
+		botBackward();
+		wait(3);
     }
 }
 
-// both motors clockwise
-void BotForward ( void )
+void wait(int x)
 {
-	PORTA &= ~(1<<PORTA1);//Bin1 pin
-	PORTA |= (1<<PORTA2);//Bin2 pin
-	PORTA |= (1<<PORTA3);//standby pin
-	PORTA &= ~(1<<PORTA4);//Ain1 pin
-	PORTA |= (1<<PORTA5);//Ain2  pin 
-	
-
+	_delay_ms(10);
 }
 
-// bothe motors counter clockwise
-void BotBackwards( void  )
+void botForward()
 {
-	PORTA |= (1<<PORTA1);//Bin1 pin
-	PORTA &= ~(1<<PORTA2);//Bin2 pin
-	PORTA |= (1<<PORTA3);//standby pin
-	PORTA |= (1<<PORTA4);//Ain1 pin
-	PORTA &= ~(1<<PORTA5);//Ain2  pin
+	setPortABit(PA1);
+	clearPortABit(PA2);
+	setPortABit(PA4);
+	clearPortABit(PA5);
+}
 
+void botBackward()
+{
+	clearPortABit(PA1);
+	setPortABit(PA2);
+	clearPortABit(PA4);
+	setPortABit(PA5);
+}
+
+void botLeft()
+{
+	setPortABit(PA1);
+	clearPortABit(PA2);
+	clearPortABit(PA4);
+	setPortABit(PA5);
+}
+
+void botRight()
+{
+	clearPortABit(PA1);
+	setPortABit(PA2);
+	setPortABit(PA4);
+	clearPortABit(PA5);
+}
+
+int	setPortABit(int x)
+{
+	PORTA |= (1 << x);
+}
+
+int	clearPortABit(int x)
+{
+	PORTA &= ~(1 << x);
+}
+
+int	togglePortABit(int x)
+{
+	PORTA ^= (1 << x);
+}
 	
 		
-		
-}
-
-//  motor1 clockewise , motor2 counter clockwise 
-void BotLeft ( void )
-{
-	PORTA &= ~(1<<PORTA1);//Bin1 pin
-	PORTA |= (1<<PORTA2);//Bin2 pin
-	PORTA |= (1<<PORTA3);//standby pin
-	PORTA |= (1<<PORTA4);//Ain1 pin
-	PORTA &= ~(1<<PORTA5);//Ain2  pin
-	 
-	
-	
-}
-
-//  motor1 clockewise , motor2 counter clockwise 
-void BotRight ( void  )
-{
-	PORTA |=(1<<PORTA1);//Bin1 pin
-	PORTA &= ~(1<<PORTA2);//Bin2 pin
-	PORTA |= (1<<PORTA3);//standby pin
-	PORTA |= (1<<PORTA4);//Ain1 pin
-	PORTA &= ~(1<<PORTA5);//Ain2  pin
-	
-		
-}
-// both motors off
-void BotStop  ( void )
-{
-		PORTA |=(1<<PORTA1);//Bin1 pin
-		PORTA |= (1<<PORTA2);//Bin2 pin
-		PORTA &= ~(1<<PORTA3);//standby pin
-		PORTA |= (1<<PORTA4);//Ain1 pin
-		PORTA |= (1<<PORTA5);//Ain2  pin
-}
+//PORTA |= (1<<PA1);
+//PORTA |= (1<<PA2);
+//PORTA |= (1<<PA4);
+//PORTA |= (1<<PA5);	
 
